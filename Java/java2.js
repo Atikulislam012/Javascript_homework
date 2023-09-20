c98d8a6b87ad78d2abbef50aceaddb8fee5c9ea7
Atikulislam012 <info.atikulislam015@gmail.com>
2023-09-20 22:01:48 +0600

    Arithmetic and Assignment Operator

---
+++ Java/java2.js
@@ -0,0 +1,29 @@
+
+// Arithmetic and Assignment Operator
+var num1 = prompt("Enter the first number :");
+var num2 = prompt("Enter the second number :");
+
+num1 = parseInt(num1, 10);
+num2 = parseInt(num2, 10);
+
+var sum,sub,Multiplication,Division,Modulo;
+
+sum = num1 + num2;
+    console.log("Sum = " + sum);
+    
+sub = num1 - num2;
+    console.log("sub = " + sub);
+
+Multiplication = num1 * num2;
+    console.log("Multiplication = " + Multiplication);
+    
+Division = num1 / num2;
+    console.log("Division = " +  Division);
+
+Modulo = num1 % num2;
+    console.log("Modulo = " + Modulo);
+
+
+
+
+
