f40d6d45b4906f00a052e62cb63eda1885ccb047
Atikulislam012 <info.atikulislam015@gmail.com>
2023-09-20 23:11:57 +0600

    Event & Odd

---
+++ Java/java3.js
@@ -0,0 +1,10 @@
+// Relaitional and Logical Operator
+
+var num1, num2;
+
+num1 = 30;
+num2 = 50;
+
+console.log(num1>num2)
+
+
---
+++ Java/java4.js
@@ -0,0 +1,22 @@
+// Event & Odd
+var num = 9;
+
+if(num%2==0){
+    console.log("Event");
+}
+else{
+    console.log("Odd");
+}
+
+// Posetive & Nagetive
+var num1 = 0;
+
+if(num1>0){
+    console.log("Positive");
+}
+if(num1<0){
+    console.log("Nagetive");
+}
+else{
+    console.log("Zero");
+}
