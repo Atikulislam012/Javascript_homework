c1875f4484be7f67940222f6dc025eede47e4c40
Atikulislam012 <info.atikulislam015@gmail.com>
2023-09-20 20:57:34 +0600

    variable called and to show Output

---
+++ Java/Java.js
@@ -0,0 +1,61 @@
+
+let collection = document.getElementsByClassName("Headtag");collection[0].style.backgroundColor = "red";
+
+//Variable Called
+var Instute,Instute2,course;
+
+Instute = "IT Name : Univeerse It Instute "
+Instute2 = " & Universe Soft Care "
+course = " Course : MERN STACK"
+
+
+
+//In The prompt
+var text = prompt("Enter Your Name:")
+console.log("Number Of The Character" + text.length)
+
+
+
+
+
+//In The Document 
+document.write("Name : Atikul Islam Rahat <br/>");
+document.write("Address : 107/1, Uttar Mugdapara, Bashabo, Dhaka <br/>");
+document.write("Email : info.atikulislam015@gmail.com <br/>");
+document.write(Instute.concat(Instute2)  + " <br/>")
+document.write(course + " <br/>")
+document.write("Phone : +8801975099574 <br/> <br/>");
+
+
+document.write("Name : Ashraful Hoque <br/>");
+document.write("Address : 304/4, Boddo Mandir, Bashabo, Dhaka <br/>");
+document.write("Email : ashraful@gmail.com <br/>");
+document.write(Instute.concat(Instute2) + " <br/>")
+document.write(course + " <br/>")
+document.write("Phone : +880192345465 <br/> <br/>");
+
+
+document.write("Name : Arafat Khan <br/>");
+document.write("Address : 143/4, Uttar Badda, Dhaka <br/>");
+document.write("Email : arafatkhat@gmail.com <br/>");
+document.write(Instute.concat(Instute2) + " <br/>")
+document.write(course + " <br/>")
+document.write("Phone : +880196656556 <br/> <br/>");
+
+document.write("Name : Imtiaz Chowdury <br/>");
+document.write("Address : C/4, Aftabragar, Rampura, Dhaka <br/>");
+document.write("Email : imtiaz65@gmail.com <br/>");
+document.write(Instute.concat(Instute2) + " <br/>")
+document.write(course + " <br/>")
+document.write("Phone : +8801934673451 <br/> <br/>");
+
+document.write("Name : Jahid Hasas <br/>");
+document.write("Address : Narayeenganj, Dhaka <br/>");
+document.write("Email : jahid345@gmail.com <br/>");
+document.write(Instute.concat(Instute2) + " <br/>")
+document.write(course + " <br/>")
+document.write("Phone : +8801934528064 <br/> <br/>");
+
+
+
+
